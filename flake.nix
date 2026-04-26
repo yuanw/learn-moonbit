6fb4958263778c11216e7cb0e8fd27a23a48eb40
yuanw <me@yuanwang.ca>
2026-04-26 17:53:29 -0600

    fix error

--- flake.nix
+++ flake.nix
@@ -19,6 +19,11 @@
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [ inputs.moonbit-overlay.overlays.default ];
+          config = {
+            problems.handlers = {
+              tcc.broken = "warn";
+            };
+          };
         };
 
         devshells.default = {
